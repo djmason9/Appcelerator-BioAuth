4de55c14ca6fd8d824be88d22cfc867cf96bff7b
Darren <djmason9@gmail.com>
2017-12-01 10:24:41 -0700

    Globalized styles and added banner

--- app/alloy.js
+++ app/alloy.js
@@ -1,2 +1,12 @@
+/* Image path and density examples
+
+/images/image.png	100×100
+/images/iphone/image@2x.png	200×200
+/images/iphone/image@3x.png	300×300
+/images/android/res-hdpi/image.png	150×150
+/images/android/res-xhdpi/image.png	200×200
+/images/android/res-xxhdpi/image.png	300×300
+
+*/
 
 Alloy.Globals.isIphoneX = (Ti.Platform.displayCaps.platformHeight == 812);
