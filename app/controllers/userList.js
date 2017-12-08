a5417b5ce1f64c9c3a328e3dfb4e655fc4a9e0f4
Darren <djmason9@gmail.com>
2017-12-08 09:58:46 -0700

    consolidated code

--- app/controllers/userList.js
+++ app/controllers/userList.js
@@ -71,22 +71,20 @@ var wasShown = false,
 	tiUserList.doUserDetail = function(e) {
 
 		var json = Alloy.Collections.userModel.at(e.itemIndex).toJSON();
-
+		var opts = {
+				"id" : json.id,
+				"name" : json.name,
+				"status" : json.status,
+				"phone" : json.phone,
+				"location" : json.location
+		},
+		controllerNm = "userDetails";
+		
 		//Used for real buttons
 		if (OS_IOS) {
-			Alloy.Globals.pushViewOnController("userDetails", {
-				"name" : json.name,
-				"status" : json.status,
-				"phone" : json.phone,
-				"location" : json.location
-			});
+			Alloy.Globals.pushViewOnController(controllerNm, opts);
 		} else {
-			var win1 = Alloy.createController("userDetails", {
-				"name" : json.name,
-				"status" : json.status,
-				"phone" : json.phone,
-				"location" : json.location
-			}).getView();
+			var win1 = Alloy.createController(controllerNm, opts).getView();
 			win1.open();
 		}
 
