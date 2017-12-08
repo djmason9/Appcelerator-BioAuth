79a6dedd16f3e80caf7e4cc540d9d84f1e8ebd7c
Darren <djmason9@gmail.com>
2017-12-08 14:47:24 -0700

    removed model code

--- app/controllers/userDetails.js
+++ app/controllers/userDetails.js
@@ -1,20 +1,4 @@
 (function main() {
-	
-	Alloy.Models.details.destroy();
-	
-	Alloy.Models.details.save({
-        "name" : $.args.name,
-        "status" : $.args.status,
-        "location" : $.args.location,
-        "phone" : $.args.phone
-    }, {
-        success : function(model, response, options) {
-            console.log("saved");
-        }
-    }, {
-        error : function(model, response, options) {
-            console.log("fail");
-        }
-    });
+
 	
 })();
